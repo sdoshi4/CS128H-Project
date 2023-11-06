2e373d012408cdfe093b82ca3ab5c8250dac61ff
Crackerjacks51 <shaandoshi4@gmail.com>
2023-11-06 01:15:26 -0600

    create file structure

---
+++ finalProject/target/debug/deps/finalProject-aab23d1e35429472.d
@@ -0,0 +1,6 @@
+C:\Users\shaan\OneDrive\Documents\Programming - New\CS128H\CS128H-Project\finalProject\target\debug\deps\finalProject-aab23d1e35429472.rmeta: src\main.rs src\Node.rs
+
+C:\Users\shaan\OneDrive\Documents\Programming - New\CS128H\CS128H-Project\finalProject\target\debug\deps\finalProject-aab23d1e35429472.d: src\main.rs src\Node.rs
+
+src\main.rs:
+src\Node.rs:
--- finalProject/target/debug/deps/finalProject.d
+++ finalProject/target/debug/deps/finalProject.d
@@ -1,5 +1,6 @@
-C:\Users\shaan\OneDrive\Documents\Programming - New\CS128H\CS128H-Project\finalProject\target\debug\deps\finalProject.exe: src\main.rs
+C:\Users\shaan\OneDrive\Documents\Programming - New\CS128H\CS128H-Project\finalProject\target\debug\deps\finalProject.exe: src\main.rs src\Node.rs
 
-C:\Users\shaan\OneDrive\Documents\Programming - New\CS128H\CS128H-Project\finalProject\target\debug\deps\finalProject.d: src\main.rs
+C:\Users\shaan\OneDrive\Documents\Programming - New\CS128H\CS128H-Project\finalProject\target\debug\deps\finalProject.d: src\main.rs src\Node.rs
 
 src\main.rs:
+src\Node.rs:
